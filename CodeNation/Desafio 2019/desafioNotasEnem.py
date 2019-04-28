498549e528b6d9e2420b22db35da42da21a7b9b6
DESKTOP-1TD3NTB\igorl <igorlpa90@gmail.com>
2019-04-28 15:24:32 -0300

    novos testes multithread

--- CodeNation/Desafio 2019/desafioNotasEnem.py
+++ CodeNation/Desafio 2019/desafioNotasEnem.py
@@ -13,19 +13,44 @@ import matplotlib.pyplot as plt
 fileTrain = pandas.read_csv('train.csv')
 fileTest = pandas.read_csv('test.csv')
 
-dataTrain = fileTrain[['NU_INSCRICAO', 'NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_MT']]
+features = ['NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC', 'NU_NOTA_REDACAO']
+features_cat = ['CO_PROVA_CN','CO_PROVA_CH','CO_PROVA_LC','CO_PROVA_MT']
 
+#TRAIN
+dataTrain = fileTrain[features]
 dataTrain.fillna(value=0, inplace=True)
+x_train = dataTrain.iloc[:,:].values
+y_train = fileTrain[['NU_NOTA_MT']].fillna(value=0)
 
-x_train = dataTrain.iloc[:,1:-1].values
+categorical_train = fileTrain[features_cat]
+categorical_train.fillna(value=0, inplace=True)
 
-y_train = dataTrain.iloc[:, -1].values
           
 
+#TESTE
+dataTest = fileTest[features]
+dataTest.fillna(value=0, inplace=True)
+x_test = dataTest.iloc[:,:].values
+
+categorical_test = fileTest[features_cat]
+categorical_test.fillna(value=0, inplace=True)
+
+#dummies
+cat =  pandas.concat([categorical_train, categorical_test], axis=0, ignore_index=True)
+dummies = pandas.get_dummies(cat, drop_first=True)
+
+dummies_train = dummies.iloc[:categorical_train.shape[0],:]
+dummies_test = dummies.iloc[categorical_train.shape[0]:,:] 
+
+
+#jutando as features
+x_train = np.append(arr = x_train, values = dummies_train.values, axis =1)
+x_test = np.append(arr = x_test, values = dummies_test.values, axis =1)
 
 
 
-      
+
+
 #SCALING + REGRESSOR
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -33,7 +58,7 @@ X = sc_X.fit_transform(x_train)
 
 # Fitting SVR to the dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf', C=400)
+regressor = SVR(kernel = 'rbf', C=100)
 # =============================================================================
 # 
 # from sklearn.ensemble import RandomForestRegressor
@@ -50,12 +75,6 @@ regressor.fit(X, y_train)
 
 
 #-------------------------------------------------------------
-#TESTE
-
-dataTest = fileTest[['NU_INSCRICAO', 'NU_NOTA_CN', 'NU_NOTA_CH', 'NU_NOTA_LC']]
-dataTest.fillna(value=0, inplace=True)
-
-x_test = dataTest.iloc[:,1:].values
 
 
 #PREDICTION
@@ -64,7 +83,7 @@ pred_DF = pandas.DataFrame(data=pred)
 
 dataTest.reset_index(drop=True, inplace=True)
 
-result =  pandas.concat([dataTest['NU_INSCRICAO'], pred_DF], axis=1, ignore_index=True)
+result =  pandas.concat([fileTest['NU_INSCRICAO'], pred_DF], axis=1, ignore_index=True)
 result.columns = ['NU_INSCRICAO', 'NU_NOTA_MT']
 
 result.to_csv('answer.csv', index=False)
